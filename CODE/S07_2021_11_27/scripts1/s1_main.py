e0ea94dded9444960818c5d256dc1c0c7622bbd4
leangaurav <leanguarav.me@gmail.com>
2021-11-27 11:32:09 +0530

    Session 7 Notebooks: functions, modules

---
+++ CODE/S07_2021_11_27/scripts1/s1_main.py
@@ -0,0 +1,6 @@
+import s2_module
+
+print("script 1")
+print("__name__=", repr(__name__), type(__name__))
+
+print(s2_module.test(1,2))
---
+++ CODE/S07_2021_11_27/scripts1/s2_module.py
@@ -0,0 +1,9 @@
+
+
+def test(a, b):
+    return a + b
+    
+if __name__ == '__main__':
+    print(test(10,20))
+    print("script 2")
+    print("__name__=", repr(__name__), type(__name__))
